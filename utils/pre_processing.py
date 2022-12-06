ba6aed73f7e4659838ed79ba69d732e0b63c1e2a
Divya Rathod <21mcs106@nith.ac.in>
2022-12-06 15:14:01 +0530

    Undo pre_processing.py for minimum sentence length

--- utils/pre_processing.py
+++ utils/pre_processing.py
@@ -11,14 +11,15 @@ def textwrite(path, data):
 #     textwrite('test.txt', ['cháu lên ba', 'cháu vô mẫu giáo'])
 
 def pop_range(x, y, min_length=0, max_length=1e10):
-##    i = 0
-    new_x, new_y = list(), list()
     print(len(x))
+    i = 0
+    new_x, new_y = list(), list()
+    
     for i in range(len(x)):
-##        if (min_length <= len(x[i]) <= max_length) and (min_length <= len(y[i]) <= max_length):
-        new_x.append(x[i])
-        new_y.append(y[i])
-##        else: i+=1
+        if (min_length <= len(x[i]) <= max_length) and (min_length <= len(y[i]) <= max_length):
+            new_x.append(x[i])
+            new_y.append(y[i])
+        else: i+=1
     
     assert len(new_x) == len(new_y)
     
