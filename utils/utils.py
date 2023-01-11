1bc73bc8055268b75e3ac07de095185b1243193d
Divya Rathod <21mcs106@nith.ac.in>
2023-01-11 12:06:52 +0530

    Changed bleu code

--- utils/utils.py
+++ utils/utils.py
@@ -31,10 +31,10 @@ def bleu_score(references, candidates):
     # reference and candidate is list of strings
     references = [[sen.split(' ')] for sen in references]
     candidates = [sen.split(' ') for sen in candidates]
-    bleu1 = corpus_bleu(references, candidates, weights=(1, 0, 0, 0)) * 100
-    bleu2 = corpus_bleu(references, candidates, weights=(0, 1, 0, 0)) * 100
-    bleu3 = corpus_bleu(references, candidates, weights=(0, 0, 1, 0)) * 100
-    bleu4 = corpus_bleu(references, candidates, weights=(0, 0, 0, 1)) * 100
+    bleu1 = corpus_bleu(references, candidates, weights=(1.0, 0, 0, 0)) * 100
+    bleu2 = corpus_bleu(references, candidates, weights=(0.5, 0.5, 0, 0)) * 100
+    bleu3 = corpus_bleu(references, candidates, weights=(0.33, 0.33, 0.33, 0)) * 100
+    bleu4 = corpus_bleu(references, candidates, weights=(0.25, 0.25, 0.25, 0.25)) * 100
 
     score = corpus_bleu(references, candidates) * 100
     return bleu1, bleu2, bleu3, bleu4, score, references, candidates
