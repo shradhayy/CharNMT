9844d33ac10645e3d10a510f0177723fe81856a3
Shradhay <aryanshradhay@outlook.com>
2023-05-17 12:10:07 +0530

    malyalam pre_processing added

--- utils/pre_processing.py
+++ utils/pre_processing.py
@@ -1,4 +1,3 @@
-import json
 import re
 
 def textwrite(path, data):
@@ -6,12 +5,7 @@ def textwrite(path, data):
         for item in data:
             f.write(item + "\n")
 
-        
-# if __name__ == "__main__":
-#     textwrite('test.txt', ['cháu lên ba', 'cháu vô mẫu giáo'])
-
 def pop_range(x, y, min_length=0, max_length=1e10):
-    print(len(x))
     i = 0
     new_x, new_y = list(), list()
     
@@ -19,31 +13,31 @@ def pop_range(x, y, min_length=0, max_length=1e10):
         if (min_length <= len(x[i]) <= max_length) and (min_length <= len(y[i]) <= max_length):
             new_x.append(x[i])
             new_y.append(y[i])
-        else: i+=1
+        else:
+            i += 1
     
     assert len(new_x) == len(new_y)
     
     return new_x, new_y
 
 def resub(data):
-    data = re.sub('\&#?[a-z0-9]+;', '', data) # remove html entity
-    # keep only english and vietnamese characters
-    data = re.sub("[^a-z0-9A-Z_ \n\r\.\'?,!ऀँंःऄअआइईउऊऋऌऍऎएऐऑऒओओऔकखगघङचछजझञटठडढणतथदधनऩपफबभमयरऱलळऴवशषसहऺऺऻ़ऽािीुूृॄॅॆेैॉॊोौ्ॎॏॐ॒॑॓॔ॕॖॗक़ख़ग़ज़ड़ढ़फ़य़ॠॡ।ॣॢ॥०१२३४५६७८९॰ॱॲॳॴॵॶॷॸॹॺॻॼॽॾॿ]", "", data)
-    data = re.sub(' +', ' ', data) # remove duplicate white space
+    data = re.sub('\&#?[a-z0-9]+;', '', data)  # remove HTML entities
+    data = re.sub("[^a-z0-9A-Z_ \n\r\.\'?,!ഀ-ഃഅ-ഌഎ-ഐഒ-ഔക-ഹാ-ൃെ-ൈൊ-്ൗൠ-ൣ]", "", data)
+    data = re.sub(' +', ' ', data)  # remove duplicate white space
     
     return data
 
 def pre_processing(x, y, min_length, max_length):
     x, y = pop_range(x, y, min_length, max_length)
-    data = '\n'.join(x+y)
+    data = '\n'.join(x + y)
     data = resub(data)
     data = data.split("\n")
-    assert len(data)%2 == 0, "error happens when split data"
-    x, y = data[:len(data)//2], data[len(data)//2:]
+    assert len(data) % 2 == 0, "Error occurred while splitting data"
+    x, y = data[:len(data) // 2], data[len(data) // 2:]
 
-    # print some samples
-    print("Some last sentences")
-    for i in range(-1,-5,-1):
-        print(x[i],'|',y[i])
+    # Print some samples
+    print("Some last sentences:")
+    for i in range(-1, -5, -1):
+        print(x[i], '|', y[i])
 
     return x, y
